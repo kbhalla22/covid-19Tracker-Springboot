d0256f0f526fc1a41b0f37b9f23c9408e1abaafe
karanbhalla <kbhalla22@gmail.com>
2020-04-11 19:09:11 -0400

    Added component for showing total cases

--- src/main/java/io/karrybee/covid19Tracker/controllers/MainController.java
+++ src/main/java/io/karrybee/covid19Tracker/controllers/MainController.java
@@ -1,11 +1,14 @@
 package io.karrybee.covid19Tracker.controllers;
 
 
+import io.karrybee.covid19Tracker.models.LocationStats;
 import io.karrybee.covid19Tracker.services.Covid19DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller//Render an html ui
 public class MainController {
@@ -13,7 +16,15 @@ public class MainController {
     Covid19DataService covid19DataService;
 @GetMapping("/")
     public String home(Model model){
-    model.addAttribute("locationStats",covid19DataService.getAllStats());
+    List<LocationStats>allStats=covid19DataService.getAllStats();
+    /*
+    Taking a list of objects, converting it into a stream, each object maps to integer value,
+    take stream and sum it up.
+     */
+    int totalReportedCases= allStats.stream().mapToInt(stat->stat.getLatestTotalCases()).sum();
+    model.addAttribute("locationStats",allStats);
+    model.addAttribute("totalReportedCases",totalReportedCases);
+
         return "home";
     }
 }
