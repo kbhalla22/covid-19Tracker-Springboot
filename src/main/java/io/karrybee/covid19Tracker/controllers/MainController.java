986d791d87ab75e254cb7c7cc2b190367b833be2
karanbhalla <kbhalla22@gmail.com>
2020-04-11 16:41:32 -0400

    Adding basic template and getmapping

--- src/main/java/io/karrybee/covid19Tracker/controllers/MainController.java
+++ src/main/java/io/karrybee/covid19Tracker/controllers/MainController.java
@@ -2,11 +2,12 @@ package io.karrybee.covid19Tracker.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller//Render an html ui
 public class MainController {
 
-
+@GetMapping("/")
     public String home(){
         return "";
     }
