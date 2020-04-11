c79c4369ac0eda998340cd38c0ad0a512cbf7fd7
karanbhalla <kbhalla22@gmail.com>
2020-04-11 16:40:05 -0400

    added controller

---
+++ src/main/java/io/karrybee/covid19Tracker/controllers/MainController.java
@@ -0,0 +1,13 @@
+package io.karrybee.covid19Tracker.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller//Render an html ui
+public class MainController {
+
+
+    public String home(){
+        return "";
+    }
+}
